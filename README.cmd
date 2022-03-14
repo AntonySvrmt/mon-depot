80399f8c274019f041d24c16d0e1214216e86340
Antony <antonysavarimoutou@gmail.com>
2022-03-14 16:20:51 +0100

    Ajout commit

--- README.cmd
+++ README.cmd
@@ -1,10 +1,6 @@
-Texte 
-texte
-teeexte
-
-
-bonjour!
-
-Hello
-Hello
-Hello
+let iceCream = 'chocolat';
+if (iceCream === 'chocolat') {
+  alert("J'adore la glace au chocolat !");
+} else {
+  alert("Ooooh, mais j'aurais préféré au chocolat.");
+}
